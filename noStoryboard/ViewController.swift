b9b43ed58ca7e58a8f8b52ec37b941255b561f5d
kevintriestocode <kwangsan@gmail.com>
2018-07-27 10:57:21 -0400

    Initial Commit

---
+++ noStoryboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  noStoryboard
+//
+//  Created by Kevin Wang on 7/27/18.
+//  Copyright © 2018 KevinWang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
