0ffb282056b03755c13b98715389c3db3298229d
kevintriestocode <kwangsan@gmail.com>
2018-08-02 11:50:47 -0400

    started following Dougly project

---
+++ noStoryboard/NumbersCollectionViewDelegateAndDataSource.swift
@@ -0,0 +1,35 @@
+//
+//  NumbersCollectionView.swift
+//  ViewsProgrammatically
+//
+//  Created by Douglas Galante on 3/23/17.
+//  Copyright © 2017 Dougly. All rights reserved.
+//
+import UIKit
+//
+class NumbersCollectionViewDelegateAndDataSource: NSObject, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let numbers: [String]
+    
+    override init() {
+        var nums: [String] = ["A"]
+        self.numbers = nums
+    }
+    
+    // MARK: - Number Of Sections
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numbers.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath) as! NumbersCollectionViewCell
+        cell.label.text = String(numbers[indexPath.row])
+        return cell
+    }
+    
+}
