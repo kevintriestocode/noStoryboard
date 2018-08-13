fba6d734f8af30ace6227264974996c4838414f7
Kevin Wang <kwangsan@gmail.com>
2018-08-13 09:53:11 -0400

    green start screen. UIStatusBarStyle in info.plist

---
+++ Pods/Target Support Files/MDFFontDiskLoader/MDFFontDiskLoader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MDFFontDiskLoader : NSObject
+@end
+@implementation PodsDummy_MDFFontDiskLoader
+@end
---
+++ Pods/Target Support Files/MDFRobotoFontLoader/MDFRobotoFontLoader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MDFRobotoFontLoader : NSObject
+@end
+@implementation PodsDummy_MDFRobotoFontLoader
+@end
