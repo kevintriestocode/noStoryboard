ed78231a7f2e0f5a5809dff1353c028ba88ea70b
Kevin Wang <kwangsan@gmail.com>
2018-09-13 10:08:24 -0400

    Finished guide 2-1

--- noStoryboard/HighlightPracticeViewController.swift
+++ noStoryboard/HighlightPracticeViewController.swift
@@ -1,65 +1,78 @@
-import Foundation
 import UIKit
-import UIKit.UIGraphicsRenderer
 import CoreGraphics
-import SnapKit
 
 public class HighlightPractiveViewController: UIViewController {
-  var textLabel: UILabel!
-  var labelText: NSAttributedString!
-  var renderer: UIGraphicsRenderer!
-
+  
   override public func viewDidLoad() {
-    labelText = NSAttributedString(string: "Highlight me.")
-    
-    renderer = UIGraphicsRenderer(bounds: view.frame)
-    
-    textLabel = UILabel()
-    textLabel.attributedText = labelText
-    textLabel.textAlignment = .center
-    
-    view.addSubview(textLabel)
-    view.backgroundColor = .white
-    
-    textLabel.snp.makeConstraints { make in
-      make.top.left.right.bottom.equalTo(view).inset(30)
-      make.center.equalTo(view.snp.center)
-    }
-
     drawRect()
-
-    // Here's where it gets dirty.
-//    let size = labelText.size()
-//    let context = NSStringDrawingContext()
-//    let rect = labelText.boundingRect(with: size, options: .usesLineFragmentOrigin, context: context)
-//
-//
-//
-//    let path = CGPath(rect: rect, transform: nil)
-//
-//    let framesetter = CTFramesetterCreateWithAttributedString(labelText)
-//    let frame = CTFramesetterCreateFrame(framesetter, range, path, nil)
-//    let lines = CTFrameGetLines(frame)
-//
-//    var origins = lines
-////    let lineOrigins = CTFrameGetLineOrigins(frame, range, origins)
   }
   
   func drawRect() {
+    // Initialize a graphics context
+    let context: CGContext = UIGraphicsGetCurrentContext()!
     
-    let size = labelText.size()
-    let rect = labelText.boundingRect(with: size, options: .usesLineFragmentOrigin, context: nil)
-    print(rect)
+    // Flip teh context coordinates, in iOS only.
+    context.translateBy(x: 0, y: view.bounds.size.height)
+    context.scaleBy(x: 1.0, y: -1.0)
     
+    // Set the text matrix.
+    /// Don't know Swift equivalent of @objc CGContextSetTextMatrix ...
+    context.textMatrix = CGAffineTransform.identity
     
+    // Create a path which bounds the area where you will be drawing text.
+    let path = CGMutablePath()
     
-    let currentColor = UIColor(red: 200/255, green: 180/255, blue: 160/255, alpha: 1)
+    // In this simple example, initialize a rectangular path.
+    /// CGRect bounds = CGRectMake(10.0, 10.0, 200.0, 200.0);
+    /// CGPathAddRect(path, NULL, bounds );
+    let bounds = CGRect(x: 10.0, y: 10.0, width: 200.0, height: 200.0)
+    path.addRect(bounds)
     
-    let bezierPath = UIBezierPath(roundedRect: rect, cornerRadius: 10)
-    print(bezierPath)
-    
-    
-    
+    // Initialize a string.
+    // CFStringRef textString = CFSTR("Hello, World! I know nothing in the world that has as much power as a word. Sometimes I write one, and I look at it, until it begins to shine.");
+    let textString = NSString(string: "Hello, World!") // CF & NS are toll-free bridged.
 
+    // Create a mutable attributed string with a max length of 0.
+    /// The max length is a hint as to how much internal storage to reserve.
+    /// 0 means no hint.
+    let attrString = CFAttributedStringCreateMutable(kCFAllocatorDefault, 0)
+    
+    // Copy the textString into the newly created attrString
+    /// CFAttributedStringReplaceString (attrString, CFRangeMake(0, 0), textString);
+    CFAttributedStringReplaceString(attrString, CFRangeMake(0, 0), textString)
+    
+    // Create a color that will be added as an attribute to the attrString.
+    /// CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    /// CGFloat components[] = { 1.0, 0.0, 0.0, 0.8 };
+    /// CGColorRef red = CGColorCreate(rgbColorSpace, components);
+    /// CGColorSpaceRelease(rgbColorSpace);
+    let rgbColorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
+    let components: [CGFloat] = [1.0, 0.0, 0.0, 0.8]
+    let red: CGColor = CGColor(colorSpace: rgbColorSpace, components: components)!
+    
+    // Set the color of the first 12 chars to red.
+    /// CFAttributedStringSetAttribute(attrString, CFRangeMake(0, 12), kCTForegroundColorAttributeName, red);
+    CFAttributedStringSetAttribute(attrString, CFRangeMake(0, 12), kCTForegroundColorAttributeName, red)
+    
+    // Create the framesetter with the attributed string.
+    /// CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString(attrString);
+    /// CFRelease(attrString); -- in swift: --> 'CFRelease' is unavailable: Core Foundation objects are automatically memory managed
+    let framesetter = CTFramesetterCreateWithAttributedString(attrString!)
+  
+    // Create a frame.
+    /// CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, 0), path, NULL);
+    let frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, 0), path, nil)
+    
+    // Draw the specified frame in the given context.
+    /// CTFrameDraw(frame, context);
+    
+    CTFrameDraw(frame, context)
+    
+    // Release the objects we used.
+    /// CFRelease(frame);
+    /// CFRelease(path);
+    /// CFRelease(framesetter);
+
+    
   }
 }
