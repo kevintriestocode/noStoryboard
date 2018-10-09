df108fec5fa197695ea1c64a738d46c3edd821d9
Kevin Wang <kwangsan@gmail.com>
2018-10-09 11:35:52 -0400

    Added MKMapView, MKPointAnnotation for zipcode

--- noStoryboard/Controller/WeatherViewController.swift
+++ noStoryboard/Controller/WeatherViewController.swift
@@ -2,18 +2,30 @@ import UIKit
 import SnapKit
 import Alamofire
 import ObjectMapper
+import MapKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, MKMapViewDelegate {
   var weatherView: WeatherView!
+  var map: MKMapView!
+  
+  var placemark: MKPointAnnotation?
   
   var settings: Settings!
   var zipcode: String!
+  
+  var lat: Double?
+  var lon: Double?
   
   var apiKey: String!
   var apiCall: String!
   
   override func viewDidLoad() {
     weatherView = WeatherView()
+    map = MKMapView(frame: .zero)
+    self.map.delegate = self
+    map.isZoomEnabled = true
+    map.isRotateEnabled = false
+    placemark = MKPointAnnotation()
     
     print("WeatherViewController did load")
     navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(popSettings))
@@ -44,6 +56,14 @@ class WeatherViewController: UIViewController {
     weatherView.snp.makeConstraints { make in
       make.edges.equalTo(view)
     }
+    weatherView.addSubview(map)
+    map.layer.cornerRadius = Configuration.Label.cornerRadius
+    map.snp.makeConstraints { make in
+      make.left.right.equalTo(view).inset(5)
+      make.top.equalTo(weatherView.lineView.snp.bottom).offset(5)
+      make.centerX.equalTo(view)
+      make.height.equalTo(view.snp.width)
+    }
   }
 
   @objc func popSettings() {
@@ -58,6 +78,7 @@ class WeatherViewController: UIViewController {
 //    Alamofire.request(URL).responseJSON { response in print(response) }
     Alamofire.request(URL).responseString { response in
       if let jsonString = response.result.value {
+        print(jsonString)
         if let responseObject = Response(JSONString: jsonString) {
           guard responseObject.cod != 401 else {
             self.weatherView.temperatureLabel.text = "401 Error: Invalid API Key"
@@ -88,6 +109,17 @@ class WeatherViewController: UIViewController {
           if let minimumTemperature = responseObject.main?.minimumTemperature?.kelvinToFarenheit() {
             self.weatherView.lowsLabel.text = minimumTemperature
           }
+          // Coordinates
+          self.lon = responseObject.coord?.longitude
+          self.lat = responseObject.coord?.latitude
+          
+          let center = CLLocationCoordinate2D(latitude: self.lat!, longitude: self.lon!)
+          let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+          
+          self.placemark?.coordinate = center
+          self.map.addAnnotation(self.placemark!)
+          
+          self.map.setRegion(MKCoordinateRegion(center: center, span: span), animated: true)
         }
       }
     }
