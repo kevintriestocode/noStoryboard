85b17b9580751263a11f1ed9480450183f76a1cc
Kevin Wang <kwangsan@gmail.com>
2018-10-12 08:59:06 -0400

    Whitespace

--- noStoryboard/Helpers/Configuration.swift
+++ noStoryboard/Helpers/Configuration.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public struct Configuration {
   public static var dataSource = [ "One", "Two", "Three", "Four", "Five" ]
-  
+
   // Adobe Color CC
   public struct Color {
     public static var backgroundColor = UIColor(red: 54/255, green: 117/255, blue: 219/255, alpha: 1)
@@ -12,7 +12,7 @@ public struct Configuration {
     public static var backgroundColorLight = UIColor(red: 48/255, green: 173/255, blue: 242/255, alpha: 1)
     public static var backgroundColorLighter = UIColor(red: 46/255, green: 215/255, blue: 232/255, alpha: 1)
   }
-  
+
   public struct Label {
     public static var cornerRadius = CGFloat(9)
   }
