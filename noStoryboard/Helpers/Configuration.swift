b233c8c6d5b881ddffdf38fa078669842673c07a
Kevin Wang <kwangsan@gmail.com>
2018-10-12 08:58:48 -0400

    Adobe Color CC

--- noStoryboard/Helpers/Configuration.swift
+++ noStoryboard/Helpers/Configuration.swift
@@ -4,8 +4,13 @@ import UIKit
 public struct Configuration {
   public static var dataSource = [ "One", "Two", "Three", "Four", "Five" ]
   
+  // Adobe Color CC
   public struct Color {
     public static var backgroundColor = UIColor(red: 54/255, green: 117/255, blue: 219/255, alpha: 1)
+    public static var backgroundColorDark = UIColor(red: 48/255, green: 71/255, blue: 242/255, alpha: 1)
+    public static var backgroundColorDarker = UIColor(red: 73/255, green: 46/255, blue: 232/255, alpha: 1)
+    public static var backgroundColorLight = UIColor(red: 48/255, green: 173/255, blue: 242/255, alpha: 1)
+    public static var backgroundColorLighter = UIColor(red: 46/255, green: 215/255, blue: 232/255, alpha: 1)
   }
   
   public struct Label {
