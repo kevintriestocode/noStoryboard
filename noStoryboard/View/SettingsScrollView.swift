c2bfbdbd4f650800113e96ee7451ca2646e1286c
kevintriestocode <kwangsan@gmail.com>
2018-10-13 08:24:21 -0400

    launch screen, settingsScrollView

---
+++ noStoryboard/View/SettingsScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsScrollView.swift
+//  noStoryboard
+//
+//  Created by Kevin Wang on 10/13/18.
+//  Copyright © 2018 KevinWang. All rights reserved.
+//
+
+import Foundation
+import SnapKit
+
+class SettingsScrollView: UIScrollView {
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
