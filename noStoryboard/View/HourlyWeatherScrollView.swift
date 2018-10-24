709b3edc13e05c7d00b157c09627cfc5ce236d61
Kevin Wang <kwangsan@gmail.com>
2018-10-24 15:03:19 -0400

    add ghetto hourlyWeatherBlocks

--- noStoryboard/View/HourlyWeatherScrollView.swift
+++ noStoryboard/View/HourlyWeatherScrollView.swift
@@ -7,9 +7,34 @@
 //
 
 import UIKit
+import SnapKit
 
 class HourlyWeatherScrollView: UIScrollView {
   convenience init() {
     self.init(frame: .zero)
+    
+    let hour0 = HourlyWeatherBlock()
+    hour0.hour.text = "0"
+    hour0.temp.text = "1º F"
+    self.addSubview(hour0)
+    hour0.snp.makeConstraints { make in
+      make.left.equalTo(self)
+    }
+
+    let hour1 = HourlyWeatherBlock()
+    hour1.hour.text = "1"
+    hour1.temp.text = "1º F"
+    self.addSubview(hour1)
+    hour1.snp.makeConstraints { make in
+      make.left.equalTo(hour0.snp.right)
+    }
+
+    let hour2 = HourlyWeatherBlock()
+    hour2.hour.text = "2"
+    hour2.temp.text = "1º F"
+    self.addSubview(hour2)
+    hour2.snp.makeConstraints { make in
+      make.left.equalTo(hour1.snp.right)
+    }
   }
 }
