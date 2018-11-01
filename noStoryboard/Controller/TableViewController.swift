6784eeedf8d3f1cead9dfc7dc7858b8109e33113
Kevin Wang <kwangsan@gmail.com>
2018-11-01 13:25:49 -0400

    settings as datasource; remove old code

--- noStoryboard/Controller/TableViewController.swift
+++ noStoryboard/Controller/TableViewController.swift
@@ -12,12 +12,17 @@ import SnapKit
 
 public class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   var tableView: UITableView!
-
+  var settings: Settings!
+  var tableData: [Any]?
+  
   let cellID = "cellID"
 
   public override func viewDidLoad() {
     print("Table View did load")
     title = "Table View"
+
+    settings = Settings.sharedSettings
+    tableData = settings.tableData
 
     setupTableView()
 
@@ -51,12 +56,12 @@ public class TableViewController: UIViewController, UITableViewDataSource, UITab
   
   public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-    cell.textLabel?.text = Configuration.dataSource[indexPath.row]
+    cell.textLabel?.text = "_"
     return cell
   }
   
   public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return Configuration.dataSource.count
+    return tableData?.count ?? 0
   }
 
   /// Asks the data source for the title of the header of the specified section of the table view.
@@ -73,16 +78,16 @@ public class TableViewController: UIViewController, UITableViewDataSource, UITab
   // MARK: Inserting or Deleting Table Rows
   public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
     if editingStyle == .delete {
-      print("Deleted row [\(indexPath.row)], item name: \(Configuration.dataSource[indexPath.row]).")
+      
 
-      Configuration.dataSource.remove(at: indexPath.item)
-      self.tableView.deleteRows(at: [indexPath], with: .automatic)
+      
+      
     }
     if editingStyle == .insert {
-      Configuration.dataSource.insert("NewThing", at: indexPath.item)
-      self.tableView.insertRows(at: [indexPath], with: .automatic)
+      
+      
 
-      print("Inserted row [\(indexPath.row)], item name: \(Configuration.dataSource[indexPath.row])")
+      
     }
   }
 
@@ -93,9 +98,9 @@ public class TableViewController: UIViewController, UITableViewDataSource, UITab
   // MARK: Reordering Table Rows
   /// Drag function.
   public func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-    let movedObjTemp = Configuration.dataSource[sourceIndexPath.item]
-    Configuration.dataSource.remove(at: sourceIndexPath.item)
-    Configuration.dataSource.insert(movedObjTemp, at: destinationIndexPath.item)
+    
+    
+    
   }
 
   // MARK: - Customizing the Table View Behavior
@@ -110,7 +115,7 @@ public class TableViewController: UIViewController, UITableViewDataSource, UITab
 
   // MARK: Managing Selections
   public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    print("You have tapped table row: \(indexPath.row), item name: \(Configuration.dataSource[indexPath.row]).") // To console each time a cell is tapped.
+  
   }
 //  public func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
 //    print("You have deselected table row: \(indexPath.row), item name: \(Configuration.dataSource[indexPath.row]).")
@@ -130,7 +135,7 @@ public class TableViewController: UIViewController, UITableViewDataSource, UITab
 
   // MARK: Reordering Table Rows
   public func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
-    print("You moved the item named: \"\(Configuration.dataSource[sourceIndexPath.row])\", at sourceIndexPath: \(sourceIndexPath) ~> toProposedIndexPath: \(proposedDestinationIndexPath)")
+    
     return proposedDestinationIndexPath
   }
 
